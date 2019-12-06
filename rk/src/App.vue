cbf8fec1d79473877542da31504508f6e6893477
OptimalRain <2440766294@qq.com>
2019-12-06 09:03:06 +0800

    'rk6-2'

--- rk/src/App.vue
+++ rk/src/App.vue
@@ -1,32 +1,44 @@
 <template>
   <div id="app">
+    <div class="con">
+      <router-view/>
+    </div>
+    
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+*{
+  margin: 0;
+  padding: 0;
+  list-style: none;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+html,body,#app{
+  width: 100%;
+  height: 100%;
+}
+#app{
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+.con{
+  flex:1;
+  overflow: auto;
+}
+#nav{
+  width: 100%;
+  height: 40px;
+  display: flex;
+  a{
+    flex:1;
+    text-align: center;
+    line-height: 40px;
   }
 }
 </style>
--- rk/src/views/Home.vue
+++ rk/src/views/Home.vue
@@ -1,18 +1,90 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <div class="banner">
+        <swiper :options='swiperOption' ref="mySwiper">
+          <swiper-slide class="item">1</swiper-slide>
+          <swiper-slide class="item">2</swiper-slide>
+          <swiper-slide class="item">3</swiper-slide>
+          <swiper-slide class="item">4</swiper-slide>
+        </swiper>
+      </div>
+      <div class="ListItem"> 
+        <div class="Item_List" v-for="(item,key) in List" :key="key">
+          <dl>
+            <dt><img :src="item.image" alt=""></dt>
+            <dd>{{item.title}}</dd>
+          </dl>
+        </div>
+      </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  name:'',
+  data () {
+    return {
+      swiperOption:{
+        loop:true,
+        autoplay:true
+      },
+      List:[
+        {title:'成单奖励',image:'http://hiphotos.baidu.com/feed/pic/item/b17eca8065380cd7092d50f2ab44ad34598281fa.jpg',id:1},
+        {title:'成单奖励',image:'http://hiphotos.baidu.com/feed/pic/item/b17eca8065380cd7092d50f2ab44ad34598281fa.jpg',id:2},
+        {title:'成单奖励',image:'http://hiphotos.baidu.com/feed/pic/item/b17eca8065380cd7092d50f2ab44ad34598281fa.jpg',id:3},
+        {title:'成单奖励',image:'http://hiphotos.baidu.com/feed/pic/item/b17eca8065380cd7092d50f2ab44ad34598281fa.jpg',id:4},
+        {title:'成单奖励',image:'http://hiphotos.baidu.com/feed/pic/item/b17eca8065380cd7092d50f2ab44ad34598281fa.jpg',id:5},
+        {title:'成单奖励',image:'http://hiphotos.baidu.com/feed/pic/item/b17eca8065380cd7092d50f2ab44ad34598281fa.jpg',id:6},
+        {title:'成单奖励',image:'http://hiphotos.baidu.com/feed/pic/item/b17eca8065380cd7092d50f2ab44ad34598281fa.jpg',id:7},
+        {title:'成单奖励',image:'http://hiphotos.baidu.com/feed/pic/item/b17eca8065380cd7092d50f2ab44ad34598281fa.jpg',id:8},
+      ]
+    };
+  },
+
+  components: {},
+
+  computed: {
+    swiper(){
+      return this.$refs.mySwiper.swiper
+    }
+  },
+//方法
+  methods: {}
+}
+
+</script>
+<style lang='scss' scoped>
+.item{
+  width: 100%;
+  height: 200px;
+  text-align: center;
+  line-height: 200px;
+}
+.ListItem{
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+}
+.Item_List{
+  flex-shrink: 0;
+  width: 20%;
+  height: 100px;
+  margin: 10px 2.5%;
+  dt{
+    width: 70px;
+    height: 70px;
+    border-radius: 50%;
+    overflow: hidden;
+    img{
+      width: 100%;
+
+    }
+  }
+  dd{
+    width: 100%;
+    text-align: center;
+    line-height: 30px;
+    font-size: 16px;
   }
 }
-</script>
+</style>
